2612cf8ddc8b42c9ce34dbfd2663a54342f9ec77
Folka2134 <folka2134@gmail.com>
2023-11-07 17:00:39 +0000

    updated styles

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full bg-gradient-to-r from-green-500 to-green-200 p-6">
-      <div className="flex w-[23rem] flex-col  gap-2">
+      <div className="flex w-[23rem] flex-col gap-2">
         <h1 className="text-center text-7xl">Folka Jobs</h1>
         <div className="flex justify-end gap-4">
           <span>Sign-up</span>
