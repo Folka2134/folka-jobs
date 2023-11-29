16b481010ce1dd25414db179e031aa4e70e3c794
Folka2134 <folka2134@gmail.com>
2023-11-29 13:36:35 +0000

    added home link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -25,7 +25,9 @@ const Navbar = () => {
   return (
     <div className="w-full bg-gradient-to-r from-green-500 to-green-200 p-6">
       <div className="flex w-[23rem] flex-col gap-2">
-        <h1 className="text-center text-7xl">Folka Jobs</h1>
+        <Link href="/">
+          <h1 className="text-center text-7xl">Folka Jobs</h1>
+        </Link>
         <div className="flex justify-end gap-4">
           <AuthButton />
         </div>
