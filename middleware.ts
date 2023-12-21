aad168646bc23bd7baba7a4912833023e7184ab7
Folka2134 <folka2134@gmail.com>
2023-12-21 14:09:16 +0000

    updated middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,18 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-  publicRoutes: ["/", "/events/:id", "/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"],
-  ignoredRoutes: ["/api/webhook/clerk", "/api/webhook/stripe", "/api/uploadthing"]
+  publicRoutes: [
+    '/',
+    '/jobs/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
  
 export const config = {
